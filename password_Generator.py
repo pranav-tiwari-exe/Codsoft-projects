b58978816d878504728f2251e7ac54ef4e13c8d8
Pranav tiwari <91013998+pranav-tiwari-exe@users.noreply.github.com>
2023-11-03 22:19:06 +0530

    Add files via upload
    A GUI password gernerator using Tkinter

--- password_Generator.py
+++ password_Generator.py
@@ -12,32 +12,35 @@ def print_password():
     l=var_1
     ANS=generate_password(l)
     l_2.config(text=("Password Generated is:"))
-    l_result.config(text=ANS,fg="red")
-    
-
+    l_result.config(text="\n"+ANS+"\n",fg="red",wraplength=200)
 
 window=Tk()
 window.title("Password Generator:")
-window.geometry('400x100+600+350')
+window.geometry('400x200+600+350')
+window.resizable(width=False,height=True)
+window.configure(bg="white")
+
+
 var_1=IntVar()
 
-l_1=Label(window,text="Enter the Length of the Password to be Generated:").pack()
+l_1=Label(window,text="PASSWORD GENERATION TOOL",font=("arial bold",15),bg="white").pack()
 
-e_1=Entry(window,textvariable=var_1).pack()
+l_=Label(window,text="\nEnter Password Length\n",font=("arial bold",10),bg="white").pack()
 
-but_1=Button(window,text=("SUBMIT"),command=print_password).pack()
+e_1=Entry(window,textvariable=var_1,width=24,relief="flat",bg="silver").pack()
 
-fr=Frame(window)
+but_1=Button(window,text=("GENERATE"),font=("calibri bold",10),command=print_password,width=20,relief='flat',bg="tomato").pack()
 
-l_2=Label(fr,text="")
+fr=Frame(window,bg="white")
 
-l_result=Label(fr,text="")
+l_2=Label(fr,text="",font=("Arial",10),bg="white")
 
-fr.pack()
+l_result=Label(fr,text="",font=("Arial",10),bg="white")
+
+fr.pack(side=BOTTOM)
 
 l_2.pack(side="left")
 
 l_result.pack(side="left")
-
 
 window.mainloop()
